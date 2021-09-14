2bde3f60ad91d159bfe70b12f0444813703103bd
tnzlr <83607411+tnzlr@users.noreply.github.com>
2021-09-14 16:58:58 +0100

    branch test 1

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+// import { StyleSheet } from "react-native";
 import Header from "./screens/Header";
 import Homepage from "./screens/Homepage";
 
@@ -13,11 +12,11 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
