334556999d770a1ca0164249871bbc3f2ddc3ff0
matt <mattworthington51@gmail.com>
2021-09-14 15:37:35 +0100

    added home screen

---
+++ screens/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function Header() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Hobbyist</Text>
+      <Button
+        title="Filter Search"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    // justifyContent: "center",
+  },
+  header: {
+    padding: 40,
+    fontSize: 40,
+  },
+});
