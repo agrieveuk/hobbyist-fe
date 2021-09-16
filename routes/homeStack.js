0db0e7a5bf79d76973a139bdc297b451c07f52e5
matt <mattworthington51@gmail.com>
2021-09-16 08:10:33 +0100

    added react navigation routes

---
+++ routes/homeStack.js
@@ -0,0 +1,31 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import Homepage from '../screens/Homepage';
+import ClubPage from '../screens/ClubPage';
+import Header from '../screens/Header';
+import MapViewPage from '../screens/MapViewPage';
+import React from 'react';
+
+const screens = {
+  Homepage: {
+    screen: Homepage,
+    navigationOptions: {
+      headerTitle: () => <Header />,
+    },
+  },
+  ClubPage: {
+    screen: ClubPage,
+  },
+  MapViewPage: {
+    screen: MapViewPage,
+  },
+};
+
+const HomeStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    headerTintColor: '#444',
+    headerStyle: { height: 175 },
+  },
+});
+
+export default createAppContainer(HomeStack);
